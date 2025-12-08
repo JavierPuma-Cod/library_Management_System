3155551b9225872dd51062b4a82deb22a57eb4ed
JavierPuma-Cod <codigos.jp.2002@gmail.com>
2025-12-08 18:45:45 -0500

    creation class conexionBD

---
+++ src/main/java/jp/project/conection/conexionBD.java
@@ -0,0 +1,13 @@
+package jp.project.conection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class conexionBD {
+    private static final String URL= "jdbc:mysql://localhost:3306/biblioteca";
+    private static final String USER= "root";
+    private static final String PASSWORD= "root";
+    public static Connection conectar() throws SQLException
+    {
+        return DriverManager.getConnection(URL,USER,PASSWORD);
+    }
+}
